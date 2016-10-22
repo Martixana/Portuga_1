438be554c8fce6aeb1bb48478ffa7a863f72a28d
Mariza Pereira <marupereira@ladmins-macbook.attlocal.net>
2016-10-22 13:57:08 -0700

    First commit

--- js/angular/app.js
+++ js/angular/app.js
@@ -4,6 +4,6 @@ myApp.controller('RegistrationController', ['$scope', function($scope)
 {
 
 	$scope.register = function(){
-		$scope.message = $scope.guest.firstname + ", your reservation for a party of " + $scope.guest.number + " has been made. Thank you."
+		$scope.message = $scope.guest.firstname + ", your reservation for a party of " + $scope.guest.number + " has been made. Thank you. " 
 	}
 }])
